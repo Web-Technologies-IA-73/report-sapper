e2b999d6751470bd8be423ee60d72751fbf357f0
maxzatsanyi <zatsanyi.max@gmail.com>
2021-03-23 12:40:14 +0200

    Зацарный 4.1

---
+++ src/routes/lab4/task_1/zatsarnyi/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import imgScreen from './screen.jpg';
+    import secScreen from './imageOne.jpg';
+</script>
+
+<img src=".{secScreen}" alt="Задание">
+<img src=".{imgScreen}" alt="Результат">
---
+++ src/routes/lab4/task_2/zatsarnyi/index.svelte
@@ -0,0 +1,11 @@
+
+<style>
+    iframe {
+        width: 100%;
+        height: 85vh;
+        margin: 0 0 1em 0;
+        border-radius: 12px;
+    }
+</style>
+
+<iframe src="https://web-technologies-ia-73.github.io/zatsarnyi_slider/index.html" title="Кошик"></iframe>
